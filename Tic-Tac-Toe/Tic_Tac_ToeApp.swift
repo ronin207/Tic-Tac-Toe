ec6acd63fc4c19aaba17a61f17e75941a208cc9d
Takumi Otsuka <takumi.ot0911@gmail.com>
2021-10-22 20:11:40 +0800

    Initial Commit

---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Takumi Otsuka on 22/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
